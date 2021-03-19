c5451368528c532019065b0b65d6562c77a248a2
ChristianSchuster <dechriss@me.com>
2021-03-19 19:09:18 +0100

    Added second content view to wrap animation block directly in SwiftUI.

--- TabViewTester/ContentView.swift
+++ TabViewTester/ContentView.swift
@@ -18,10 +18,11 @@ class ViewModel: ObservableObject {
     
     
     func startAutocycle() {
-        self.cycleTimer = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { (timer) in
+        self.cycleTimer = Timer.scheduledTimer(withTimeInterval: 3.0, repeats: true) { (timer) in
             print("Tab thru timer fired.")
             self.autocycle()
         }
+        
         
     }
     
@@ -38,7 +39,7 @@ class ViewModel: ObservableObject {
     
     func autocycle() {
         
-        withAnimation(.interactiveSpring()) {
+        withAnimation(.easeInOut(duration: 2.9)) {
             if ( self.tabSelection < 3 ) {
                 self.tabSelection += 1
             } else {
@@ -54,7 +55,15 @@ struct ContentView: View {
     @StateObject var model: ViewModel = ViewModel()
     
     var body: some View {
-        
+        VStack {
+            
+            Spacer()
+            
+            Text("Version 1")
+                .font(.largeTitle)
+            
+            Spacer()
+            
         TabView(selection: self.$model.tabSelection)  {
                     Text("Tab Content 1").tabItem { Text("Tab Label 1") }.tag(1)
                         .padding()
@@ -71,6 +80,56 @@ struct ContentView: View {
         .onAppear(){
             self.model.startAutocycle()
         }
+        }
+    
+    }
+}
+
+struct ContentView2: View {
+    
+    @StateObject var model: ViewModel = ViewModel()
+    
+    @State var tabSelection: Int = 0
+    
+    var body: some View {
+        
+        VStack {
+            
+            Spacer()
+            
+            Text("Version 2")
+                .font(.largeTitle)
+            
+            Spacer()
+            
+            TabView(selection: self.$tabSelection)  {
+                        Text("Tab Content 1").tabItem { Text("Tab Label 1") }.tag(1)
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 25.0, style: .continuous).foregroundColor(.blue))
+                        Text("Tab Content 2").tabItem { Text("Tab Label 2") }.tag(2)
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 25.0, style: .continuous).foregroundColor(.green))
+                        Text("Tab Content 3").tabItem { Text("Tab Label 3") }.tag(3)
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 25.0, style: .continuous).foregroundColor(.red))
+                    }
+            .tabViewStyle(PageTabViewStyle())
+            .foregroundColor(.white)
+            
+            .onAppear(){
+                self.model.startAutocycle()
+            }
+            .onChange(of: self.model.tabSelection, perform: { value in
+                
+                withAnimation(.easeInOut(duration: 2.9)) {
+                    self.tabSelection = value
+                }
+                
+        })
+        }
+        
+//
+//        })
         
     
     }
@@ -78,6 +137,9 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView2()
+        }
     }
 }
